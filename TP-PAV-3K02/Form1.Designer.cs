050a7cc75de96d1478d365d3ed1af9a48c30bb3b
Hassan Mafud <hassanmafud_97@hotmail.com>
2019-08-29 10:48:54 -0300

    estructura del form Suscriptores

---
+++ TP-PAV-3K02/Form1.Designer.cs
@@ -0,0 +1,391 @@
+﻿namespace TP_PAV_3K02
+{
+    partial class suscriptores
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblsuscriptores = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCodigoPostal = new System.Windows.Forms.Label();
+            this.lblCalle = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblNroDoc = new System.Windows.Forms.Label();
+            this.lblTipoDoc = new System.Windows.Forms.Label();
+            this.lblApellido = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.cmbTipoDoc = new System.Windows.Forms.ComboBox();
+            this.txtNroDoc = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtCalle = new System.Windows.Forms.TextBox();
+            this.cmbProvincia = new System.Windows.Forms.ComboBox();
+            this.cmbLocalidad = new System.Windows.Forms.ComboBox();
+            this.cmbCodigoPostal = new System.Windows.Forms.ComboBox();
+            this.lblLocalidad = new System.Windows.Forms.Label();
+            this.lblProvincia = new System.Windows.Forms.Label();
+            this.lblPais = new System.Windows.Forms.Label();
+            this.cmbPais = new System.Windows.Forms.ComboBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.grillaSuscriptores = new System.Windows.Forms.DataGridView();
+            this.lblBuscar = new System.Windows.Forms.Label();
+            this.cmbBuscar = new System.Windows.Forms.ComboBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.grillaSuscriptores)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblsuscriptores
+            // 
+            this.lblsuscriptores.AutoSize = true;
+            this.lblsuscriptores.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblsuscriptores.Location = new System.Drawing.Point(17, 19);
+            this.lblsuscriptores.Name = "lblsuscriptores";
+            this.lblsuscriptores.Size = new System.Drawing.Size(107, 23);
+            this.lblsuscriptores.TabIndex = 0;
+            this.lblsuscriptores.Text = "Suscriptores";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(56, 58);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(63, 16);
+            this.lblNombre.TabIndex = 1;
+            this.lblNombre.Text = "Nombre :";
+            // 
+            // lblCodigoPostal
+            // 
+            this.lblCodigoPostal.AutoSize = true;
+            this.lblCodigoPostal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoPostal.Location = new System.Drawing.Point(56, 246);
+            this.lblCodigoPostal.Name = "lblCodigoPostal";
+            this.lblCodigoPostal.Size = new System.Drawing.Size(99, 16);
+            this.lblCodigoPostal.TabIndex = 2;
+            this.lblCodigoPostal.Text = "Codigo Postal :";
+            // 
+            // lblCalle
+            // 
+            this.lblCalle.AutoSize = true;
+            this.lblCalle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalle.Location = new System.Drawing.Point(56, 183);
+            this.lblCalle.Name = "lblCalle";
+            this.lblCalle.Size = new System.Drawing.Size(45, 16);
+            this.lblCalle.TabIndex = 3;
+            this.lblCalle.Text = "Calle :";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(56, 213);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(62, 16);
+            this.lblNumero.TabIndex = 4;
+            this.lblNumero.Text = "Numero :";
+            this.lblNumero.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblNroDoc
+            // 
+            this.lblNroDoc.AutoSize = true;
+            this.lblNroDoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNroDoc.Location = new System.Drawing.Point(56, 152);
+            this.lblNroDoc.Name = "lblNroDoc";
+            this.lblNroDoc.Size = new System.Drawing.Size(61, 16);
+            this.lblNroDoc.TabIndex = 5;
+            this.lblNroDoc.Text = "Nro Doc:";
+            // 
+            // lblTipoDoc
+            // 
+            this.lblTipoDoc.AutoSize = true;
+            this.lblTipoDoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoDoc.Location = new System.Drawing.Point(56, 122);
+            this.lblTipoDoc.Name = "lblTipoDoc";
+            this.lblTipoDoc.Size = new System.Drawing.Size(70, 16);
+            this.lblTipoDoc.TabIndex = 6;
+            this.lblTipoDoc.Text = "Tipo Doc :";
+            this.lblTipoDoc.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // lblApellido
+            // 
+            this.lblApellido.AutoSize = true;
+            this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido.Location = new System.Drawing.Point(56, 88);
+            this.lblApellido.Name = "lblApellido";
+            this.lblApellido.Size = new System.Drawing.Size(64, 16);
+            this.lblApellido.TabIndex = 7;
+            this.lblApellido.Text = "Apellido :";
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(138, 54);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(138, 20);
+            this.txtnombre.TabIndex = 8;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(138, 84);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(138, 20);
+            this.txtApellido.TabIndex = 9;
+            // 
+            // cmbTipoDoc
+            // 
+            this.cmbTipoDoc.FormattingEnabled = true;
+            this.cmbTipoDoc.Location = new System.Drawing.Point(138, 117);
+            this.cmbTipoDoc.Name = "cmbTipoDoc";
+            this.cmbTipoDoc.Size = new System.Drawing.Size(138, 21);
+            this.cmbTipoDoc.TabIndex = 10;
+            this.cmbTipoDoc.Text = "DNI";
+            this.cmbTipoDoc.SelectedIndexChanged += new System.EventHandler(this.cmbTipoDoc_SelectedIndexChanged);
+            // 
+            // txtNroDoc
+            // 
+            this.txtNroDoc.Location = new System.Drawing.Point(138, 148);
+            this.txtNroDoc.Name = "txtNroDoc";
+            this.txtNroDoc.Size = new System.Drawing.Size(138, 20);
+            this.txtNroDoc.TabIndex = 11;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(138, 209);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(138, 20);
+            this.txtNumero.TabIndex = 12;
+            // 
+            // txtCalle
+            // 
+            this.txtCalle.Location = new System.Drawing.Point(138, 179);
+            this.txtCalle.Name = "txtCalle";
+            this.txtCalle.Size = new System.Drawing.Size(138, 20);
+            this.txtCalle.TabIndex = 13;
+            // 
+            // cmbProvincia
+            // 
+            this.cmbProvincia.FormattingEnabled = true;
+            this.cmbProvincia.Location = new System.Drawing.Point(138, 300);
+            this.cmbProvincia.Name = "cmbProvincia";
+            this.cmbProvincia.Size = new System.Drawing.Size(138, 21);
+            this.cmbProvincia.TabIndex = 14;
+            this.cmbProvincia.Text = "Còrdoba";
+            // 
+            // cmbLocalidad
+            // 
+            this.cmbLocalidad.FormattingEnabled = true;
+            this.cmbLocalidad.Location = new System.Drawing.Point(138, 272);
+            this.cmbLocalidad.Name = "cmbLocalidad";
+            this.cmbLocalidad.Size = new System.Drawing.Size(138, 21);
+            this.cmbLocalidad.TabIndex = 15;
+            this.cmbLocalidad.Text = "La Calera";
+            // 
+            // cmbCodigoPostal
+            // 
+            this.cmbCodigoPostal.FormattingEnabled = true;
+            this.cmbCodigoPostal.Location = new System.Drawing.Point(174, 241);
+            this.cmbCodigoPostal.Name = "cmbCodigoPostal";
+            this.cmbCodigoPostal.Size = new System.Drawing.Size(138, 21);
+            this.cmbCodigoPostal.TabIndex = 16;
+            this.cmbCodigoPostal.Text = "5016";
+            // 
+            // lblLocalidad
+            // 
+            this.lblLocalidad.AutoSize = true;
+            this.lblLocalidad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocalidad.Location = new System.Drawing.Point(56, 277);
+            this.lblLocalidad.Name = "lblLocalidad";
+            this.lblLocalidad.Size = new System.Drawing.Size(74, 16);
+            this.lblLocalidad.TabIndex = 17;
+            this.lblLocalidad.Text = "Localidad :";
+            // 
+            // lblProvincia
+            // 
+            this.lblProvincia.AutoSize = true;
+            this.lblProvincia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProvincia.Location = new System.Drawing.Point(56, 305);
+            this.lblProvincia.Name = "lblProvincia";
+            this.lblProvincia.Size = new System.Drawing.Size(70, 16);
+            this.lblProvincia.TabIndex = 18;
+            this.lblProvincia.Text = "Provincia :";
+            this.lblProvincia.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPais
+            // 
+            this.lblPais.AutoSize = true;
+            this.lblPais.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPais.Location = new System.Drawing.Point(56, 333);
+            this.lblPais.Name = "lblPais";
+            this.lblPais.Size = new System.Drawing.Size(41, 16);
+            this.lblPais.TabIndex = 19;
+            this.lblPais.Text = "Pais :";
+            // 
+            // cmbPais
+            // 
+            this.cmbPais.FormattingEnabled = true;
+            this.cmbPais.Location = new System.Drawing.Point(138, 328);
+            this.cmbPais.Name = "cmbPais";
+            this.cmbPais.Size = new System.Drawing.Size(138, 21);
+            this.cmbPais.TabIndex = 20;
+            this.cmbPais.Text = "Argentina";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(59, 378);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 35);
+            this.btnAgregar.TabIndex = 21;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Location = new System.Drawing.Point(283, 378);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(75, 35);
+            this.btnEditar.TabIndex = 22;
+            this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(174, 378);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 35);
+            this.btnEliminar.TabIndex = 23;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // grillaSuscriptores
+            // 
+            this.grillaSuscriptores.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.grillaSuscriptores.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grillaSuscriptores.Location = new System.Drawing.Point(412, 130);
+            this.grillaSuscriptores.Name = "grillaSuscriptores";
+            this.grillaSuscriptores.Size = new System.Drawing.Size(342, 283);
+            this.grillaSuscriptores.TabIndex = 24;
+            // 
+            // lblBuscar
+            // 
+            this.lblBuscar.AutoSize = true;
+            this.lblBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBuscar.Location = new System.Drawing.Point(409, 54);
+            this.lblBuscar.Name = "lblBuscar";
+            this.lblBuscar.Size = new System.Drawing.Size(56, 16);
+            this.lblBuscar.TabIndex = 25;
+            this.lblBuscar.Text = "Buscar :";
+            // 
+            // cmbBuscar
+            // 
+            this.cmbBuscar.FormattingEnabled = true;
+            this.cmbBuscar.Location = new System.Drawing.Point(497, 49);
+            this.cmbBuscar.Name = "cmbBuscar";
+            this.cmbBuscar.Size = new System.Drawing.Size(138, 21);
+            this.cmbBuscar.TabIndex = 26;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(497, 87);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(191, 20);
+            this.txtBuscar.TabIndex = 27;
+            // 
+            // suscriptores
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.cmbBuscar);
+            this.Controls.Add(this.lblBuscar);
+            this.Controls.Add(this.grillaSuscriptores);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.cmbPais);
+            this.Controls.Add(this.lblPais);
+            this.Controls.Add(this.lblProvincia);
+            this.Controls.Add(this.lblLocalidad);
+            this.Controls.Add(this.cmbCodigoPostal);
+            this.Controls.Add(this.cmbLocalidad);
+            this.Controls.Add(this.cmbProvincia);
+            this.Controls.Add(this.txtCalle);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.txtNroDoc);
+            this.Controls.Add(this.cmbTipoDoc);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.lblApellido);
+            this.Controls.Add(this.lblTipoDoc);
+            this.Controls.Add(this.lblNroDoc);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.lblCalle);
+            this.Controls.Add(this.lblCodigoPostal);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblsuscriptores);
+            this.Name = "suscriptores";
+            this.Text = "Suscriptores";
+            this.Load += new System.EventHandler(this.suscriptores_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grillaSuscriptores)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblsuscriptores;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblCodigoPostal;
+        private System.Windows.Forms.Label lblCalle;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblNroDoc;
+        private System.Windows.Forms.Label lblTipoDoc;
+        private System.Windows.Forms.Label lblApellido;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.ComboBox cmbTipoDoc;
+        private System.Windows.Forms.TextBox txtNroDoc;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtCalle;
+        private System.Windows.Forms.ComboBox cmbProvincia;
+        private System.Windows.Forms.ComboBox cmbLocalidad;
+        private System.Windows.Forms.ComboBox cmbCodigoPostal;
+        private System.Windows.Forms.Label lblLocalidad;
+        private System.Windows.Forms.Label lblProvincia;
+        private System.Windows.Forms.Label lblPais;
+        private System.Windows.Forms.ComboBox cmbPais;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.DataGridView grillaSuscriptores;
+        private System.Windows.Forms.Label lblBuscar;
+        private System.Windows.Forms.ComboBox cmbBuscar;
+        private System.Windows.Forms.TextBox txtBuscar;
+    }
+}
+
