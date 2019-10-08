a69e8c0df6fea1748f1a39d1e63fea538a0d0d76
Gonzalo Ribero <gonzaloribero98@gmail.com>
2019-10-08 10:01:11 -0300

    Planes

---
+++ TP-PAV-3K02/Modulos/Planes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TP_PAV_3K02.Repositorios;
+using TP_PAV_3K02.Modelos;
+
+namespace TP_PAV_3K02.Modulos
+{
+    public partial class Planes : Form
+    {
+
+        PlanesRepositorio _planesRepos;
+
+        public Planes()
+        {
+            InitializeComponent();
+            _planesRepos = new PlanesRepositorio();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var plan = new Plan();
+
+            plan.cod_Plan = 10;
+            plan.cod_int = 00;
+            plan.fechaInicial = DateTime.Today;
+            plan.fechaFin = DateTime.Today.AddYears(1);
+            plan.Precio = 200;
+
+            MessageBox.Show.AcceptButton("¿Estas seguro/a de elejir este plan?")
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            var plan = new Plan();
+
+            plan.cod_Plan = 20;
+            plan.cod_int = 01;
+            plan.fechaInicial = DateTime.Today;
+            plan.fechaFin = DateTime.Today.AddYears(1);
+            plan.Precio = 400;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            var plan = new Plan();
+
+            plan.cod_Plan = 30;
+            plan.cod_int = 02;
+            plan.fechaInicial = DateTime.Today;
+            plan.fechaFin = DateTime.Today.AddYears(1);
+            plan.Precio = 500;
+        }
+    }
+}
---
+++ TP-PAV-3K02/Repositorios/PlanesRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP_PAV_3K02.BaseDatos;
+using TP_PAV_3K02.Modelos;
+
+namespace TP_PAV_3K02.Repositorios
+{
+    public class PlanesRepositorio
+    {
+        Editorial_BD _BD;
+
+        public PlanesRepositorio()
+        {
+            _BD = new Editorial_BD;
+        }
+
+        public DataTable obtenerPlanes()
+        {
+            string sqlText = $"SELECT * FROM Planes";
+
+            return _BD.consulta(sqlText);
+        }
+
+        public DataTable obtenerPlanesByCod(int cod)
+        {
+            string sqlText = $"SELECT * FROM Planes WHERE cod_plan =" + cod;
+
+            return _BD.consulta(sqlText); 
+        }
+
+        public bool guardar(Plan plan)
+        {
+            string sqlText = $"INSERT [dbo].[Planes] ([cod_Plan], [cod_Int], [fecha_inicio], [fecha_fin], [precio])" +
+                $"VALUES('{plan.cod_Plan}', '{plan.cod_int}', '{plan.fechaInicial}', '{plan.fechaFin}', '{plan.Precio}')";
+            return _BD.EjecutarSQL(sqlText);
+        }
+
+    }
+}
